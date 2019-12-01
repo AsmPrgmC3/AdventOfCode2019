b4f1522ab09fab85560730431997a446da380c15
AsmPrgmC3 <asmprgmc3@gmail.com>
2019-12-01 17:03:03 +0100

    Day1 Part2

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -1,16 +1,43 @@
 ﻿open Utils.Utils
 
+
 let calculateFuel =
     flip (/) 3
     >> flip (-) 2
 
-[<EntryPoint>]
-let main _ = 
-    let lines = readFile "input1.txt"
+
+let rec calculateExtraFuel fuel =
+    let extra = calculateFuel fuel
+    if extra <= 0
+        then 0
+        else extra + calculateExtraFuel extra
+
+
+let calculateTotalFuel mass =
+    let fuel = calculateFuel mass
+    let extra = calculateExtraFuel fuel
+    fuel + extra
+
+
+let part1 lines =
     lines
     |> Seq.map int
     |> Seq.map calculateFuel
     |> Seq.sum
-    |> printfn "Required Fuel: %d"
+
+
+let part2 lines =
+    lines
+    |> Seq.map int
+    |> Seq.map calculateTotalFuel
+    |> Seq.sum
+
+
+[<EntryPoint>]
+let main _ = 
+    let lines = readFile "input1.txt"
+    
+    printfn "Required Fuel by modules: %d" <| part1 lines
+    printfn "Total required Fuel: %d" <| part2 lines
     
     0 // return an integer exit code
