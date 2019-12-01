56334848e5610d3b6e87be1ea014f0e830f82e0c
AsmPrgmC3 <asmprgmc3@gmail.com>
2019-12-01 16:44:32 +0100

    Day1 Part1

---
+++ Day1/Program.fs
@@ -0,0 +1,16 @@
+﻿open Utils.Utils
+
+let calculateFuel =
+    flip (/) 3
+    >> flip (-) 2
+
+[<EntryPoint>]
+let main _ = 
+    let lines = readFile "input1.txt"
+    lines
+    |> Seq.map int
+    |> Seq.map calculateFuel
+    |> Seq.sum
+    |> printfn "Required Fuel: %d"
+    
+    0 // return an integer exit code
---
+++ Utils/Utils.fs
@@ -0,0 +1,8 @@
+﻿module Utils.Utils
+
+open System.IO
+
+let flip f y x = f x y
+
+let readFile path =
+    File.ReadLines path
