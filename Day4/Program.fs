e273f9b42d961e920f86812657ef63d6f7a432c5
AsmPrgmC3 <asmprgmc3@gmail.com>
2019-12-04 13:17:00 +0100

    Day4 Part1

---
+++ Day4/Program.fs
@@ -0,0 +1,19 @@
+﻿open Utils.Utils
+
+
+[<EntryPoint>]
+let main _ = 
+    let [| lower; upper|] = (readFileContent "input1.txt")
+                                .Split('-')
+                                |> Array.take 2
+                                |> Array.map int
+    
+    seq { for num in lower..upper -> num }
+    |> Seq.map string
+    |> Seq.map Seq.pairwise
+    |> Seq.filter (Seq.exists (fun (a, b) -> a = b))
+    |> Seq.filter (not << Seq.exists (fun (a, b) -> b < a))
+    |> Seq.length
+    |> printfn "Possible passwords: %d"
+    
+    0 // return an integer exit code
