82e914cfb98eb15e29c3bb939010d58553194d84
AsmPrgmC3 <asmprgmc3@gmail.com>
2019-12-04 13:32:23 +0100

    Day4 Part2

--- Day4/Program.fs
+++ Day4/Program.fs
@@ -8,12 +8,21 @@ let main _ =
                                 |> Array.take 2
                                 |> Array.map int
     
-    seq { for num in lower..upper -> num }
-    |> Seq.map string
-    |> Seq.map Seq.pairwise
-    |> Seq.filter (Seq.exists (fun (a, b) -> a = b))
-    |> Seq.filter (not << Seq.exists (fun (a, b) -> b < a))
+    let part1Numbers = seq { for num in lower..upper -> num }
+                      |> Seq.map string
+                      |> Seq.map (fun num -> num, Seq.pairwise num)
+                      |> Seq.filter (fun (_, pairs) -> pairs |> Seq.exists (fun (a, b) -> a = b))
+                      |> Seq.filter (fun (_, pairs) -> pairs |> (not << Seq.exists (fun (a, b) -> b < a)))
+                      |> Seq.map (fun (num, _) -> num)
+    
+    part1Numbers
     |> Seq.length
-    |> printfn "Possible passwords: %d"
+    |> printfn "Possible passwords (part 1): %d"
+    
+    part1Numbers
+    |> Seq.map (Seq.countBy id)
+    |> Seq.filter (Seq.exists ((=) 2 << snd))
+    |> Seq.length
+    |> printfn "Possible passwords (part 2): %d"
     
     0 // return an integer exit code
