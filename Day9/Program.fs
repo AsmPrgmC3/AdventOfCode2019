99d97261d32186220e0f391caee738539da3a182
Sven Neumann <sven.neumann1@ostfalia.de>
2019-12-10 09:08:22 +0100

    Day9 Part2

--- Day9/Program.fs
+++ Day9/Program.fs
@@ -2,10 +2,16 @@
 
 [<EntryPoint>]
 let main _ = 
+    let prgm = IntcodeComputer.readInMemory "input1.txt"
     
-    let memory = IntcodeComputer.newMemory (IntcodeComputer.readInMemory "input1.txt")
+    let memory = IntcodeComputer.newMemory prgm
     IntcodeComputer.runProgram [1L] [] memory 0
     |> List.last
     |> printfn "BOOST keycode: %d"
+    
+    let memory = IntcodeComputer.newMemory prgm
+    IntcodeComputer.runProgram [2L] [] memory 0
+    |> List.last
+    |> printfn "Coordinates: %d"
     
     0 // return an integer exit code
